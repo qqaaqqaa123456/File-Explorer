e2ba54bf308b2d6b781f1af095b6b66cbe35078b
yh200212121212 <yh20021212@126.com>
2016-10-02 19:27:36 +0800

    upload some codes

---
+++ App1/pages/items/items.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    "use strict";
+
+    var ui = WinJS.UI;
+
+    ui.Pages.define("/pages/items/items.html", {
+        // 初始化该页面时将调用此函数。
+        init: function (element, options) {
+            this.itemInvoked = ui.eventHandler(this._itemInvoked.bind(this));
+        },
+
+        // 每当用户导航至该页面时都要调用此函数。
+        ready: function (element, options) {
+        },
+
+        // 此功能更新页面布局以响应布局更改。
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO:  响应布局的更改。
+        },
+
+        _itemInvoked: function (args) {
+            var groupKey = Data.groups.getAt(args.detail.itemIndex).key;
+            WinJS.Navigation.navigate("/pages/split/split.html", { groupKey: groupKey });
+        }
+    });
+})();
---
+++ App1/pages/split/split.js
@@ -0,0 +1,153 @@
+﻿(function () {
+    "use strict";
+
+    var binding = WinJS.Binding;
+    var nav = WinJS.Navigation;
+    var ui = WinJS.UI;
+    var utils = WinJS.Utilities;
+
+    ui.Pages.define("/pages/split/split.html", {
+
+        _group: null,
+        /// <field type="WinJS.Binding.List" />
+        _items: null,
+        _itemSelectionIndex: -1,
+        _wasSingleColumn: false,
+
+        // 初始化该页面时将调用此函数。
+        init: function (element, options) {
+            // 存储有关此页将显示的组和选择内容的
+            // 信息。
+            this._group = Data.resolveGroupReference(options.groupKey);
+            this._items = Data.getItemsFromGroup(this._group);
+            this._itemSelectionIndex = (options && "selectedIndex" in options) ? options.selectedIndex : -1;
+            this.itemDataSource = this._items.dataSource;
+            this.selectionChanged = ui.eventHandler(this._selectionChanged.bind(this));
+        },
+
+        // 每当用户导航至该页面时都要调用此函数。
+        ready: function (element, options) {
+            element.querySelector("header[role=banner] .pagetitle").textContent = this._group.title;
+
+            this._updateVisibility(element);
+            if (this._isSingleColumn()) {
+                this._wasSingleColumn = true;
+                if (this._itemSelectionIndex >= 0) {
+                    // 有关单列详细视图，请加载本文。
+                    binding.processAll(element.querySelector(".articlesection"), this._items.getAt(this._itemSelectionIndex));
+                }
+            } else {
+                // 如果此页包含 selectionIndex，则使该选择在 ListView 中
+                // 显示。
+                var listView = element.querySelector(".itemlist").winControl;
+                listView.selection.set(Math.max(this._itemSelectionIndex, 0));
+            }
+        },
+
+        unload: function () {
+            this._items.dispose();
+        },
+
+        updateLayout: function (element) {
+            var isSingleColumn = this._isSingleColumn();
+            if (this._wasSingleColumn === isSingleColumn) {
+                return;
+            }
+
+            var listView = element.querySelector(".itemlist").winControl;
+            var firstVisible = listView.indexOfFirstVisible;
+            this._updateVisibility(element);
+
+            var handler = function (e) {
+                listView.removeEventListener("contentanimating", handler, false);
+                e.preventDefault();
+            }
+
+            if (isSingleColumn) {
+                listView.selection.clear();
+                if (this._itemSelectionIndex >= 0) {
+                    // 如果应用程序已对齐一个单列详细信息视图，
+                    // 请向 backstack 中添加单列列表视图。
+                    nav.history.current.state = {
+                        groupKey: this._group.key,
+                        selectedIndex: this._itemSelectionIndex
+                    };
+                    nav.history.backStack.push({
+                        location: "/pages/split/split.html",
+                        state: { groupKey: this._group.key }
+                    });
+                    element.querySelector(".articlesection").focus();
+                } else {
+                    listView.addEventListener("contentanimating", handler, false);
+                    if (firstVisible >= 0 && listView.itemDataSource.list.length > 0) {
+                        listView.indexOfFirstVisible = firstVisible;
+                    }
+                    listView.forceLayout();
+                }
+            } else {
+                // 如果应用程序已取消对齐两列视图，请删除已添加到 backstack 中的所有
+                // splitPage 实例。
+                if (nav.canGoBack && nav.history.backStack[nav.history.backStack.length - 1].location === "/pages/split/split.html") {
+                    nav.history.backStack.pop();
+                }
+
+                listView.addEventListener("contentanimating", handler, false);
+                if (firstVisible >= 0 && listView.itemDataSource.list.length > 0) {
+                    listView.indexOfFirstVisible = firstVisible;
+                }
+                listView.forceLayout();
+                listView.selection.set(this._itemSelectionIndex >= 0 ? this._itemSelectionIndex : Math.max(firstVisible, 0));
+            }
+
+            this._wasSingleColumn = isSingleColumn;
+        },
+
+        // 此功检查是否应在单独的页面上显示而不是
+        // 并行显示列表和详细信息列。
+        _isSingleColumn: function () {
+            return document.documentElement.offsetWidth <= 767;
+        },
+
+        _selectionChanged: function (args) {
+            var listView = args.currentTarget.winControl;
+            var details;
+            // 默认情况下，该选择限制为单一项。
+            listView.selection.getItems().done(function updateDetails(items) {
+                if (items.length > 0) {
+                    this._itemSelectionIndex = items[0].index;
+                    if (this._isSingleColumn()) {
+                        // 如果使用对齐或纵向视图，则导航到包含
+                        // 选定项详细信息的新页面。
+                        setImmediate(function () {
+                            nav.navigate("/pages/split/split.html", { groupKey: this._group.key, selectedIndex: this._itemSelectionIndex });
+                        }.bind(this));
+                    } else {
+                        // 如果全屏或已填充，请使用新数据更新详细信息列。
+                        details = document.querySelector(".articlesection");
+                        binding.processAll(details, items[0].data);
+                        details.scrollTop = 0;
+                    }
+                }
+            }.bind(this));
+        },
+
+        // 此功能基于当前查看状态和项选择切换
+        // 两个列的可见性。
+        _updateVisibility: function (element) {
+            var splitPage = element.querySelector(".splitpage");
+            if (this._isSingleColumn()) {
+                if (this._itemSelectionIndex >= 0) {
+                    utils.addClass(splitPage, "itemdetail");
+                    element.querySelector(".articlesection").focus();
+                } else {
+                    utils.addClass(splitPage, "groupdetail");
+                    element.querySelector(".itemlist").focus();
+                }
+            } else {
+                utils.removeClass(splitPage, "groupdetail");
+                utils.removeClass(splitPage, "itemdetail");
+                element.querySelector(".itemlist").focus();
+            }
+        }
+    });
+})();
